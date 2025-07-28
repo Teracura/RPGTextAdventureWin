5ec678d473f85517362400a7dd714785142c2078
teracura <abdallahelbhary39@gmail.com>
2025-07-28 20:31:01 +0300

    Refactor
    refactor project names and data template datatypes

---
+++ RPGTextAdventureWin.UI/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using RPGTextAdventureWin.Views;
+
+namespace RPGTextAdventureWin;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    
+    private async void OnStartGameClick(object? sender, EventArgs e)
+    {
+        //TODO: Separate UI from Desktop and Android
+        await Shell.Current.GoToAsync(nameof(HeroChoosePage)); //should be windows and mac only (maybe later on support linux with a third party)
+    }
+}
