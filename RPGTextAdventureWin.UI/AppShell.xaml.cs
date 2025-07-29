19e5bd09c9d0f0dafe7a6521af73ab9d2b514982
teracura <abdallahelbhary39@gmail.com>
2025-07-29 03:22:40 +0300

    Add theme toggle button with icon updates and handling logic

--- RPGTextAdventureWin.UI/AppShell.xaml.cs
+++ RPGTextAdventureWin.UI/AppShell.xaml.cs
@@ -8,6 +8,8 @@ public partial class AppShell : Shell
     public AppShell()
     {
         InitializeComponent();
+        ChangeThemeLogos();
+
         Routing.RegisterRoute(nameof(HeroChoosePage), typeof(HeroChoosePage));
         Routing.RegisterRoute(nameof(NewGameHeroTypeSelectPage), typeof(NewGameHeroTypeSelectPage));
         Routing.RegisterRoute(nameof(ChooseSaveFileTypeSelectPage), typeof(ChooseSaveFileTypeSelectPage));
@@ -19,5 +21,40 @@ public partial class AppShell : Shell
         Routing.RegisterRoute(nameof(InventoryMenu), typeof(InventoryMenu));
         Routing.RegisterRoute(nameof(EquipmentSlotMenu), typeof(EquipmentSlotMenu));
         Routing.RegisterRoute(nameof(SpecialAbilitiesMenu), typeof(SpecialAbilitiesMenu));
+    }
+
+    private void ChangeThemeLogos()
+    {
+        if (Application.Current == null) return;
+
+        var effectiveTheme = (Application.Current.UserAppTheme != AppTheme.Unspecified)
+            ? Application.Current.UserAppTheme
+            : AppInfo.RequestedTheme;
+
+        ThemeButton.IconImageSource = effectiveTheme switch
+        {
+            AppTheme.Light => "moon.png",
+            AppTheme.Dark => "sun.png",
+            _ => "dotnet_bot.png"
+        };
+    }
+
+
+    private void OnToggleThemeClicked(object sender, EventArgs e)
+    {
+        if (Application.Current == null) return;
+        var current = (Application.Current.UserAppTheme != AppTheme.Unspecified)
+            ? Application.Current.UserAppTheme
+            : AppInfo.RequestedTheme;
+
+
+        Application.Current.UserAppTheme = current switch
+        {
+            AppTheme.Light => AppTheme.Dark,
+            AppTheme.Dark => AppTheme.Light,
+            _ => AppTheme.Dark
+        };
+
+        ChangeThemeLogos();
     }
 }
